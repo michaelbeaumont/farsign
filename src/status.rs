4162297f6c14f07e7b843fa5f5849f3c0f8c8c83
Michael Beaumont <mjboamail@gmail.com>
2020-11-22 20:43:04 +0100

    Add type constraints to StatusLights

--- src/status.rs
+++ src/status.rs
@@ -1,10 +1,15 @@
+use crate::hal::prelude::*;
+use void::Void;
+
 pub struct StatusLights<R, G, B> {
     red: R,
     green: G,
     blue: B,
 }
 
-impl<R, G, B> StatusLights<R, G, B> {
+impl<R: OutputPin<Error = Void>, G: OutputPin<Error = Void>, B: OutputPin<Error = Void>>
+    StatusLights<R, G, B>
+{
     pub fn new(red: R, green: G, blue: B) -> Self {
         StatusLights { red, green, blue }
     }
