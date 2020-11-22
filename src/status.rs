4bfbc14090ccd9ca049a648f889e372aeca8ad38
Michael Beaumont <mjboamail@gmail.com>
2020-11-22 20:26:11 +0100

    Add on_short function

--- src/status.rs
+++ src/status.rs
@@ -8,4 +8,10 @@ impl<R, G, B> StatusLights<R, G, B> {
     pub fn new(red: R, green: G, blue: B) -> Self {
         StatusLights { red, green, blue }
     }
+
+    pub fn on_short(&mut self) {
+        self.red.set_low().unwrap();
+        self.green.set_low().unwrap();
+        self.blue.set_high().unwrap();
+    }
 }
