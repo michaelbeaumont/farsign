7619d75eb9d95ff40ef17d2eb1f1e1a347fa8495
Michael Beaumont <mjboamail@gmail.com>
2021-01-20 21:53:14 +0100

    Add Timer to MorseMachine

--- src/machine.rs
+++ src/machine.rs
@@ -1,3 +1,5 @@
+use crate::hal::{pac::TIM2, prelude::*, rcc::Rcc, time::MicroSeconds, timer::Timer};
+
 #[derive(PartialEq, Eq)]
 pub enum StateChange {
     LongPress,
@@ -5,18 +7,27 @@ pub enum StateChange {
     NewLetter(u8),
 }
 
-pub struct MorseMachine {}
+pub struct MorseMachine {
+    timer: Timer<TIM2>,
+}
 
 impl MorseMachine {
-    pub fn new() -> Self {
-        MorseMachine {}
+    pub fn new(timer: TIM2, rcc: &mut Rcc, tick: MicroSeconds) -> Self {
+        Self {
+            timer: timer.timer(tick, rcc),
+        }
     }
 
-    pub fn press(&mut self) {}
+    pub fn press(&mut self) {
+        self.timer.listen();
+    }
 
-    pub fn release(&mut self) {}
+    pub fn release(&mut self) {
+        // We still need the timer running for the timeout
+    }
 
     pub fn tick(&mut self) -> Option<StateChange> {
+        self.timer.clear_irq();
         None
     }
 }
