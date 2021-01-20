0c65791bd68a8b9cd79022f09c41dbe5c25a1d5c
Michael Beaumont <mjboamail@gmail.com>
2021-01-20 21:49:35 +0100

    Add Button struct

--- src/machine.rs
+++ src/machine.rs
@@ -1,3 +1,68 @@
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum PressType {
+    Short,
+    Long,
+    VeryLong,
+}
+
+#[derive(PartialEq, Eq)]
+enum State {
+    Press(PressType),
+    WaitingOnPress,
+    Idle,
+}
+
+struct Button {
+    long_press: u32,
+    very_long_press: u32,
+    timeout: u32,
+    count: u32,
+    pressed: bool,
+}
+
+impl Button {
+    fn new(long_press: u32, very_long_press: u32, timeout: u32) -> Self {
+        Self {
+            long_press,
+            very_long_press,
+            timeout,
+            count: 0,
+            pressed: false,
+        }
+    }
+    fn release(&mut self) {
+        self.count = 0;
+        self.pressed = false;
+    }
+
+    fn press(&mut self) {
+        self.count = 0;
+        self.pressed = true;
+    }
+
+    fn state(&self) -> State {
+        if !self.pressed {
+            if self.count > self.timeout {
+                State::Idle
+            } else {
+                State::WaitingOnPress
+            }
+        } else {
+            State::Press(if self.count > self.very_long_press {
+                PressType::VeryLong
+            } else if self.count > self.long_press {
+                PressType::Long
+            } else {
+                PressType::Short
+            })
+        }
+    }
+
+    fn tick(&mut self) {
+        self.count += 1;
+    }
+}
+
 #[derive(PartialEq, Eq)]
 pub enum Transition {
     Long,
